d358de414489848c0655128c3193ce0f7e99ba43
ConnerKT <connerthompson121@gmail.com>
2023-04-27 14:16:35 -0500

    footer error patch

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,12 @@ class Footer extends Component {
                 }
                 style={{ display: "inline" }}
               >
-               
+                <span
+                  className=""
+                  data-icon="twemoji-flag-for-flag-united-kingdom"
+                  data-inline="false"
+                  id={window.$primaryLanguageIconId}
+                ></span>
               </div>
               <div 
                 onClick={() =>
@@ -40,7 +45,12 @@ class Footer extends Component {
                 }
                 style={{ display: "inline" }}
               >
-              
+                <span
+                  className=""
+                  data-icon="twemoji-flag-for-flag-poland"
+                  data-inline="false"
+                  id={window.$secondaryLanguageIconId}
+                ></span>
               </div>
             </div>
             <div className="col">
